093369cf1df3a68e79af089d0469cd60e07f3a71
chletsosa <Armstrongxyz13@yahoo.com>
2020-03-13 08:10:14 -0400

    stored json data in MySQL Database

--- server/main.js
+++ server/main.js
@@ -1,12 +1,12 @@
-const axios = require('axios');
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
-// const data = require('./data.json');
-
+const mysql = require('mysql');
+const data = require('../data.json');
+const axios = require('axios');
 const app = express();
-
+const _ = require('underscore');
 const port = 5000;
 const distDir = path.join(__dirname + '/../dist/sikfliks/');
 
@@ -22,9 +22,21 @@ const YelpReq = {
   }
 }
 
+const connection = mysql.createConnection({
+  host : "localhost",
+  port : "3306",
+  user : "chletsosa2",
+  password : "purplescarf",
+  database : "sikfliks",
+});
+
+
+const YelpData = data;
+
 app.get('/api/yelp', (req, res) => {
   axios(YelpReq).then((response) => {
     console.log(response.data);
+    //YelpData = response.data;
     fs.writeFileSync('data.json',JSON.stringify(response.data, null, 2));
     res.json(response.data);
   }).catch((error) => {
@@ -34,6 +46,32 @@ app.get('/api/yelp', (req, res) => {
 
   //res.json(data);
 
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected to local Database!');
+
+
+  const sql = "INSERT INTO delis (store, rating) VALUES ?";
+  var values =  [];
+
+
+
+  for(var i=1; i<YelpData.businesses.length; i++) {
+    values.push([YelpData.businesses[i].name,YelpData.businesses[i].rating]);
+  }
+  console.log(YelpData.busineseslength);
+  connection.query(sql, [values], function(err, result) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log("Success");
+  });
 });
 
 app.use(express.static(distDir));
